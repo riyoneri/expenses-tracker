a2a461efee25d3392d4198c8904918bbc564c75e
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-18 21:17:54 +0200

    feat: combine 2 navigators

--- app.tsx
+++ app.tsx
@@ -1,63 +1,87 @@
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import AddExpenseButton from "./components/add-expense-button";
 import { GlobalStyles } from "./constants/styles";
 import AllExpensesScreen from "./screens/all-expenses.screen";
+import ManageExpense from "./screens/manage-expense";
 import RecentExpensesScreen from "./screens/recent-expenses.screen";
 
-const Tab = createBottomTabNavigator();
+export type StackParameterList = {
+  ExpensesOverview: undefined;
+  ManageExpense: undefined;
+};
+
+export type TabParameterList = {
+  RecentExpenses: undefined;
+  AllExpenses: undefined;
+};
+
+const Tab = createBottomTabNavigator<TabParameterList>();
+const Stack = createStackNavigator<StackParameterList>();
+
+function ExpensesOverview() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerRightContainerStyle: {
+          paddingHorizontal: 10,
+        },
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          color: "white",
+        },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        sceneStyle: {
+          backgroundColor: GlobalStyles.colors.primary700,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="RecentExpenses"
+        options={{
+          title: "Recent Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" color={color} size={size} />
+          ),
+          tabBarLabel: "Recent",
+        }}
+        component={RecentExpensesScreen}
+      />
+      <Tab.Screen
+        name="AllExpenses"
+        options={{
+          title: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" color={color} size={size} />
+          ),
+        }}
+        component={AllExpensesScreen}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          headerRightContainerStyle: {
-            paddingHorizontal: 10,
-          },
-          headerStyle: {
-            backgroundColor: GlobalStyles.colors.primary500,
-          },
-          headerTitleAlign: "center",
-          headerTitleStyle: {
-            color: "white",
-          },
-          tabBarActiveTintColor: GlobalStyles.colors.accent500,
-          tabBarStyle: {
-            backgroundColor: GlobalStyles.colors.primary500,
-          },
-          sceneStyle: {
-            backgroundColor: GlobalStyles.colors.primary700,
-          },
-          headerRight: () => (
-            <AddExpenseButton color="white" name="add" onPress={() => {}} />
-          ),
-        }}
-      >
-        <Tab.Screen
-          name="RecentExpenses"
+      <Stack.Navigator>
+        <Stack.Screen
           options={{
-            title: "Recent Expenses",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="hourglass" color={color} size={size} />
-            ),
-            tabBarLabel: "Recent",
+            headerShown: false,
           }}
-          component={RecentExpensesScreen}
+          name="ExpensesOverview"
+          component={ExpensesOverview}
         />
-        <Tab.Screen
-          name="AllExpenses"
-          options={{
-            title: "All Expenses",
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="calendar" color={color} size={size} />
-            ),
-          }}
-          component={AllExpensesScreen}
-        />
-      </Tab.Navigator>
+        <Stack.Screen name="ManageExpense" component={ManageExpense} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
