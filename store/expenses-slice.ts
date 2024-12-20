b80adff81dc05d4a47c6c334c0fe328c54d53436
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-20 10:15:45 +0200

    feat: create expense store slice

---
+++ store/expenses-slice.ts
@@ -0,0 +1,44 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+import { Expense } from "../api";
+import EXPENSES from "../data/expenses";
+
+interface InitialState {
+  expenses: Expense[];
+}
+
+const initialState: InitialState = {
+  expenses: EXPENSES,
+};
+
+const expenseSlice = createSlice({
+  name: "expenses",
+  initialState,
+  reducers: {
+    addExpense: (state, action: PayloadAction<Omit<Expense, "id">>) => {
+      state.expenses.push({ ...action.payload, id: `${Date.now()}` });
+    },
+    deleteExpense: (state, action: PayloadAction<string>) => {
+      state.expenses = state.expenses.filter(
+        (expense) => expense.id !== action.payload,
+      );
+    },
+    updateExpense: (
+      state,
+      action: PayloadAction<{ id: string; updates: Omit<Expense, "id"> }>,
+    ) => {
+      const expenseIndex = state.expenses.findIndex(
+        (expense) => expense.id === action.payload.id,
+      );
+
+      state.expenses[expenseIndex] = {
+        id: action.payload.id,
+        ...action.payload.updates,
+      };
+    },
+  },
+});
+
+export default expenseSlice.reducer;
+
+export const expenseActions = expenseSlice.actions;
